0bb42af21d8ce62d65f51b20d30de9bcad429155
JulissaJ <julissa.jansen@gmail.com>
2014-04-03 11:17:53 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Faceboot::Application.config.secret_key_base = '2e93dcef2b4962403e2b6be2af69a09d557c93eb55653bb11236cb03b08d21bc18067219cc0a2de87c8c2f0dbf93902f71a905ea3e022978c00a4d0ba80b46d3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Faceboot::Application.config.session_store :cookie_store, key: '_faceboot_session'
