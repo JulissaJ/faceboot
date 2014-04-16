29c91cda7f84f08ddea58e6ffdba310425d262c5
JulissaJ <julissa.jansen@gmail.com>
2014-04-16 15:08:43 -0400

    added image to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   has_many :posts
   has_many :comments, through: :posts
+  mount_uploader :image, ImageUploader
 
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
